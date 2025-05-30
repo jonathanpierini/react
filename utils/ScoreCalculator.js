c5e4a510752ca30e631fa31aef181dc58ec1cdf7
jonathanpierini <jonathanpierini150783@gmail.com>
2025-05-30 12:00:10 +0200

    Update ScoreCalculator.js

--- utils/ScoreCalculator.js
+++ utils/ScoreCalculator.js
@@ -1,1 +1,22 @@
-// Funzione per sommare punteggi ACT e selezionare dominantPole
+export function calculateScores(answers) {
+  const scores = {
+    accettazione: 0,
+    defusione: 0,
+    sé: 0,
+    valori: 0,
+    azione: 0,
+    presenza: 0,
+  };
+
+  answers.forEach(({ pole }) => {
+    if (scores[pole] !== undefined) {
+      scores[pole]++;
+    }
+  });
+
+  return scores;
+}
+
+export function getDominantPole(scores) {
+  return Object.entries(scores).reduce((a, b) => (b[1] > a[1] ? b : a))[0];
+}
