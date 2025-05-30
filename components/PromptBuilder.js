0566893fd21023f1900824cf83624467b3db3445
jonathanpierini <jonathanpierini150783@gmail.com>
2025-05-30 12:00:45 +0200

    Update PromptBuilder.js

--- components/PromptBuilder.js
+++ components/PromptBuilder.js
@@ -1,1 +1,7 @@
-// PromptBuilder che modula ACT dinamicamente
+import promptSet from '../data/prompt_master_structured.json';
+
+export function buildPrompt(dominantPole, userMessage) {
+  const polePrompt = promptSet[dominantPole] || promptSet["default"];
+
+  return `${polePrompt.intro}\n\nUtente: ${userMessage}\n\nAssistente:`;
+}
